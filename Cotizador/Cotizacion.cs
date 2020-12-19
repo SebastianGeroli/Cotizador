cbea326f4f5a50a5692d0d21561865fb25f19515
Sebastian Geroli <sebastiangeroli@outlook.com>
2020-12-19 09:49:35 -0300

    Creacion clases Vendedor y Cotizacion

---
+++ Cotizador/Cotizacion.cs
@@ -0,0 +1,12 @@
+﻿namespace Cotizador
+{
+	class Cotizacion
+	{
+		private int m_numeroIdentificacion;
+		private string m_fechaHora;
+		private string m_codigoVendedor;
+		private Prenda m_prendaCotizada;
+		private int m_cantidadDeUnidades;
+		private int m_resultadoCotizacion;
+	}
+}
---
+++ Cotizador/Vendedor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cotizador
+{
+	class Vendedor
+	{
+		private string m_nombre;
+		private string m_apellido;
+		private string m_codigoVendedor;
+		private List<Cotizacion> HistorialDeCotizaciones;
+	}
+}
