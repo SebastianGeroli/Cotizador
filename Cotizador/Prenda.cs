6a1b2b02961390ec45b978cc1767768146483aea
Sebastian Geroli <sebastiangeroli@outlook.com>
2020-12-19 09:50:31 -0300

    Creacion clases Prenda y TiendaDeRopa

---
+++ Cotizador/Prenda.cs
@@ -0,0 +1,9 @@
+﻿namespace Cotizador
+{
+	class Prenda
+	{
+		private decimal m_precio;
+		private bool m_calidadPremium;
+		private int m_cantidadDeUnidades;
+	}
+}
---
+++ Cotizador/TiendaDeRopa.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Cotizador
+{
+	class TiendaDeRopa {
+		private string m_nombre;
+		private string m_direccion;
+		private List<Prenda> m_prendasParaVender;
+
+
+		public decimal CalcularCotizacion() {
+			decimal resultado = 0;
+			return resultado;
+		}
+	}
+}
