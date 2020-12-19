a52935fc4cb4304db35b071f468515e758039312
Sebastian Geroli <sebastiangeroli@outlook.com>
2020-12-19 12:44:45 -0300

    Constructores Vendedor y Cotizacion

--- Cotizador/Cotizacion.cs
+++ Cotizador/Cotizacion.cs
@@ -3,10 +3,26 @@
 	class Cotizacion
 	{
 		private int m_numeroIdentificacion;
-		private string m_fechaHora;
+		private string m_fecha;
 		private string m_codigoVendedor;
 		private Prenda m_prendaCotizada;
 		private int m_cantidadDeUnidades;
-		private int m_resultadoCotizacion;
+		private decimal m_resultadoCotizacion;
+
+		public int NumeroIdentificacion => m_numeroIdentificacion;
+		public string Fecha => m_fecha;
+		public string CodigoVendedor => m_codigoVendedor;
+		public Prenda PrendaCotizada => m_prendaCotizada;
+		public int CantidadDeUnidades => m_cantidadDeUnidades;
+		public decimal ResultadoCotizacion => m_resultadoCotizacion;
+
+		public Cotizacion(int numeroIdentificacion, string codigoVendedor, Prenda prendaCotizada, int cantidadDeunidades, decimal resultadoCotizacion)
+		{
+			m_numeroIdentificacion = numeroIdentificacion;
+			m_codigoVendedor = codigoVendedor;
+			m_prendaCotizada = prendaCotizada;
+			m_cantidadDeUnidades = cantidadDeunidades;
+			m_resultadoCotizacion = resultadoCotizacion;
+		}
 	}
 }
--- Cotizador/Vendedor.cs
+++ Cotizador/Vendedor.cs
@@ -11,6 +11,16 @@ namespace Cotizador
 		private string m_nombre;
 		private string m_apellido;
 		private string m_codigoVendedor;
-		private List<Cotizacion> HistorialDeCotizaciones;
+		private List<Cotizacion> HistorialDeCotizaciones = new List<Cotizacion>();
+
+		public string Nombre => m_nombre;
+		public string Apellidp => m_apellido;
+		public string CodigoVendedor => m_codigoVendedor;
+
+		public Vendedor(string nombre, string apellido, string codigoVendedor) {
+			m_nombre = nombre;
+			m_apellido = apellido;
+			m_codigoVendedor = codigoVendedor;
+		}
 	}
 }
